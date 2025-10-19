a48c90bd5dafb0910856a16313c9db7972bf5d7b
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2025-10-19 10:06:56 -0500

    Started working on Spring Security

---
+++ src/main/java/com/acompletenoobsmoke/springsecurity/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.acompletenoobsmoke.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+
+    @GetMapping
+    public String sayWelcome() {
+        return "Welcome to Spring Security Application";
+    }
+}
