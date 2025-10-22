6093f22adafb7f2e366934946831146bc8da84a4
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2025-10-22 11:27:56 -0500

    Worked on UserDetailsService & PasswordEncoder

--- src/main/java/com/acompletenoobsmoke/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/acompletenoobsmoke/springsecurity/config/ProjectSecurityConfig.java
@@ -4,6 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
@@ -31,9 +38,22 @@ public class ProjectSecurityConfig {
         http.authorizeHttpRequests(request -> request
                 .requestMatchers(apiList.stream().toArray(String[]::new)).authenticated()
                 .requestMatchers("/", "/myNotice", "/myContact", "/error").permitAll());
-        http.formLogin(withDefaults());
+        http.formLogin(flc -> flc.disable());
         http.httpBasic(withDefaults());
         System.out.println("SWAMP IZZO");
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("user").password("$2a$12$irwCLh.ny8AGu.3yUEeZhu1cUcUjl7RikfUwBL8NvikrSzJidY8b2").authorities("read").build();
+        UserDetails adminDetails = User.withUsername("admin").password("$2a$12$E78kKK9KoScVMmC1ZxsbZ.h4nTvPY4gmlAdIypgZwYv1oTgY49bZG").authorities("admin").build();
+        return new InMemoryUserDetailsManager(userDetails, adminDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
