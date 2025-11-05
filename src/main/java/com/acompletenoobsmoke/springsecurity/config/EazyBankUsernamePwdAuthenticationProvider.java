7b9efef8fa5a7cf2c081bd871d09f3d6fedbe8e1
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2025-11-05 15:33:07 -0600

    Added Authentication bean

---
+++ src/main/java/com/acompletenoobsmoke/springsecurity/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,44 @@
+package com.acompletenoobsmoke.springsecurity.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+    /**
+     * @param authentication
+     * @return
+     * @throws AuthenticationException
+     */
+
+    private final UserDetailsService easyBankUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDetails userDetails = easyBankUserDetailsService.loadUserByUsername(username);
+        if (!passwordEncoder.matches(password, userDetails.getPassword()))
+            throw new BadCredentialsException("Bad credentials");
+        return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+    }
+
+    /**
+     * @param authentication
+     * @return
+     */
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
